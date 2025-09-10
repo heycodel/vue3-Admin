1a4e6f2d1f0aec4217ffdcb3317a1ad2fc99f8f8
chl <chlfantasy@163.com>
2025-09-10 18:05:08 +0800

    登录逻辑

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,2 +1,22 @@
 //创建用户相关的小仓库
 import { defineStore } from "pinia";
+import { reqLogin } from "../../api/user";
+//引入数据类型
+import type { loginForm } from "../../api/user/type";
+const useUserStore = defineStore("user", {
+    state: () => {
+        return {
+            
+        }
+    },
+    actions: {
+       async userLogin(data:loginForm) {
+            const res = await reqLogin(data)
+            console.log(res)
+        }
+    },
+    getters: { 
+
+    }
+});
+export default useUserStore;
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,37 +1,42 @@
 <template>
     <div class="login-form">
-        <el-form :model="form" ref="formRef" label-width="100px" @submit.prevent="handleSubmit">
-            <el-form-item label="用户名" prop="username" :rules="[{ required: true, message: '请输入用户名', trigger: 'blur' }]">
-                <el-input v-model="form.username" placeholder="请输入用户名"></el-input>
+        <el-form :model="form" ref="formRef" label-width="100px">
+            <el-form-item label="用户名" prop="userName" :rules="[{ required: true, message: '请输入用户名', trigger: 'blur' }]">
+                <el-input v-model="form.userName" placeholder="请输入用户名"></el-input>
             </el-form-item>
             <el-form-item label="密码" prop="password" :rules="[{ required: true, message: '请输入密码', trigger: 'blur' }]">
                 <el-input type="password" v-model="form.password" placeholder="请输入密码"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" native-type="submit">登录</el-button>
+                <el-button type="primary" native-type="submit" @click="login">登录</el-button>
             </el-form-item>
         </el-form>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
+import useUserStore from '../../store/modules/user'
+const userStore = useUserStore()
 
 export default defineComponent({
     setup() {
         const form = ref({
-            username: '',
+            userName: '',
             password: ''
         });
-
-        const handleSubmit = () => {
+        let loginForm = reactive({
+            userName: 'admin',
+            password: '111111'
+        })
+        const login = () => {
             // 处理登录逻辑
-            console.log('提交的表单数据:', form.value);
+            userStore.userLogin(loginForm)
         };
 
         return {
             form,
-            handleSubmit
+            login
         }
     }
 })
